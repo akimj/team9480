6f1b0413b5ca6922c6a255bb632071d5c0324f18
MatthijsDV2004 <128654615+MatthijsDV2004@users.noreply.github.com>
2023-11-15 17:22:21 -0800

    test

--- slides.py
+++ slides.py
@@ -1,2 +1,15 @@
-print('hello group')
-print('Whats Up')
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap5
+import random
+from PIL import Image
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'csumb-otter'
+
+bootstrap = Bootstrap5(app)
+@app.route('/')
+def index():
+   
+    return render_template('index.html')
+
+
