9237ca22005dbdc2d3221ff639e1aac4a6abfb98
MatthijsDV2004 <128654615+MatthijsDV2004@users.noreply.github.com>
2023-11-27 17:40:04 -0800

    Commit 2
    Added upload functionality

--- slides.py
+++ slides.py
@@ -1,14 +1,19 @@
+import os
 from flask import Flask, render_template, url_for, request
 from flask_bootstrap import Bootstrap5
 import random
 from PIL import Image
-
+UPLOAD_FOLDER = 'static/images'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.config['SECRET_KEY'] = 'csumb-otter'
 
 bootstrap = Bootstrap5(app)
 @app.route('/')
 def index(): 
+    clear_uploaded_files()
+
     return render_template('index.html')
 @app.route('/slideshow')
 def slideshow():
@@ -19,6 +24,16 @@ def upload_file():
     if request.method == 'POST':
         uploaded_file = request.files['fileToUpload']
         if uploaded_file:
-            uploaded_file.save(uploaded_file.filename)  # Save the uploaded file in the current directory
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename))  # Save the uploaded file in the current directory
             return 'File uploaded successfully!'
     return 'No file uploaded'
+def clear_uploaded_files():
+    # Clear uploaded files in the uploads folder
+    folder = app.config['UPLOAD_FOLDER']
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f"Error deleting {file_path}: {e}")
