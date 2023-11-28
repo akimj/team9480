4944f45b924bf2a967a4561c1ee11d59e2582b1d
MatthijsDV2004 <128654615+MatthijsDV2004@users.noreply.github.com>
2023-11-27 17:29:10 -0800

    update

--- slides.py
+++ slides.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_bootstrap import Bootstrap5
 import random
 from PIL import Image
@@ -8,13 +8,17 @@ app.config['SECRET_KEY'] = 'csumb-otter'
 
 bootstrap = Bootstrap5(app)
 @app.route('/')
-def index():
-   
+def index(): 
     return render_template('index.html')
-
 @app.route('/slideshow')
 def slideshow():
    
     return render_template('slideshow.html')
-
-
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        uploaded_file = request.files['fileToUpload']
+        if uploaded_file:
+            uploaded_file.save(uploaded_file.filename)  # Save the uploaded file in the current directory
+            return 'File uploaded successfully!'
+    return 'No file uploaded'
