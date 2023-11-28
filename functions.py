03723e37f69f972ef12cc89b3d6bd744e8c47f71
jakemartin24 <jakmartin@csumb.edu>
2023-11-27 17:51:10 -0800

    addtext
    started on addtext

--- functions.py
+++ functions.py
@@ -1,6 +1,21 @@
-def addText():
-    return
-def addTheme(theme):
-    return
-def addMusic(song):
-    return
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
+
+def addText(index):
+
+    img = Image.open(slidephoto[index])
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("sans-serif.ttf", 16)
+
+    # if color is => 100:
+    draw.text((0, 0), slidetext[index] ,(255,255,255),font=font)
+    img.save(f 'slide' + index + '.jpg')
+
+    return none
+
+# def addTheme(theme):
+#     return
+# def addMusic(song):
+#     return
