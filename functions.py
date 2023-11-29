7caaf2aefb6a2d190ee17ff6a5460ef9a397ecd8
jakemartin24 <jakmartin@csumb.edu>
2023-11-29 15:00:12 -0800

    captions working
    captions work, along with the luminance testing
    they come out very small and can only take jpg so far, but they work. I'll try to fix those two things soon

--- functions.py
+++ functions.py
@@ -5,15 +5,31 @@ from PIL import ImageDraw
 
 def addText(index):
 
-    img = Image.open(slidephoto[index])
+    img = Image.open(f'static/images/{slidephoto[index]}.jpg')
+
+    #need to figure out: accept jpg jpeg and png
+    
+    lumi = [((a[0]*299 + a[1]*587 + a[2]*114 )//1000) for a in img.getdata()]
+    average = sum(lumi) / len(lumi)
+
+    width, height = img.size
+    captionx = width/6
+    captiony = height - (height/6)
+
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("sans-serif.ttf", 16)
+    
+    if average >= 200:
+        draw.text((captionx, captiony), f'{slidetext[index]}' ,(0,0,0))
+    else:
+        draw.text((captionx, captiony), f'{slidetext[index]}' ,(255,255,255))
 
-    # if color is => 100:
-    draw.text((0, 0), slidetext[index] ,(255,255,255),font=font)
-    img.save(f"slide{index}.jpg")
+    #need to figure out: make text bigger/scalable 
+    
+    img.save(f'slide{index}.png')
 
-    return none
+    return f'slide{index}.png'
+
+
 
 # def addTheme(theme):
 #     return
