5ea3889c7d69edc97c2f2f1b0079e09f35161598
jakemartin24 <jakmartin@csumb.edu>
2023-11-29 15:30:56 -0800

    Update functions.py
    accepts jpg and png

--- functions.py
+++ functions.py
@@ -5,29 +5,31 @@ from PIL import ImageDraw
 
 def addText(index):
 
-    img = Image.open(f'static/images/{slidephoto[index]}.jpg')
-
-    #need to figure out: accept jpg jpeg and png
-    
-    lumi = [((a[0]*299 + a[1]*587 + a[2]*114 )//1000) for a in img.getdata()]
-    average = sum(lumi) / len(lumi)
-
-    width, height = img.size
-    captionx = width/6
-    captiony = height - (height/6)
-
-    draw = ImageDraw.Draw(img)
-    
-    if average >= 200:
-        draw.text((captionx, captiony), f'{slidetext[index]}' ,(0,0,0))
+    try:
+        img = Image.open(f'static/images/{slidephoto[index]}.jpg')
+    except:
+        img = Image.open(f'static/images/{slidephoto[index]}.png')
     else:
-        draw.text((captionx, captiony), f'{slidetext[index]}' ,(255,255,255))
 
-    #need to figure out: make text bigger/scalable 
-    
-    img.save(f'slide{index}.png')
+        lumi = [((a[0]*299 + a[1]*587 + a[2]*114 )//1000) for a in img.getdata()]
+        average = sum(lumi) / len(lumi)
 
-    return f'slide{index}.png'
+        width, height = img.size
+        captionx = width/6
+        captiony = height - (height/6)
+
+        draw = ImageDraw.Draw(img)
+
+        if average >= 200:
+            draw.text((captionx, captiony), f'{slidetext[index]}' ,(0,0,0))
+        else:
+            draw.text((captionx, captiony), f'{slidetext[index]}' ,(255,255,255))
+
+        #need to figure out: make text bigger/scalable 
+        
+        img.save(f'slide{index}.png')
+
+        return f'slide{index}.png'
 
 
 
