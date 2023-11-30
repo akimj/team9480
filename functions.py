45d39ac44ab63415aa56d9996e0c45d70fbf967f
jakemartin24 <jakmartin@csumb.edu>
2023-11-29 17:21:20 -0800

    addtext should accept all image types now

--- functions.py
+++ functions.py
@@ -2,35 +2,29 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 
-
 def addText(index):
 
-    try:
-        img = Image.open(f'static/images/{slidephoto[index]}.jpg')
-    except:
-        img = Image.open(f'static/images/{slidephoto[index]}.png')
+    img = Image.open(f'static/images/{slidephoto[index]}')
+
+    lumi = [((a[0]*299 + a[1]*587 + a[2]*114 )//1000) for a in img.getdata()]
+    average = sum(lumi) / len(lumi)
+
+    width, height = img.size
+    captionx = width/6
+    captiony = height - (height/8)
+
+    draw = ImageDraw.Draw(img)
+
+    if average >= 200:
+        draw.text((captionx, captiony), f'{slidetext[index]}' ,(0,0,0))
     else:
+        draw.text((captionx, captiony), f'{slidetext[index]}' ,(255,255,255))
 
-        lumi = [((a[0]*299 + a[1]*587 + a[2]*114 )//1000) for a in img.getdata()]
-        average = sum(lumi) / len(lumi)
+    #need to figure out: make text bigger/scalable 
 
-        width, height = img.size
-        captionx = width/6
-        captiony = height - (height/8)
+    img.save(f'slide{index}.png')
 
-        draw = ImageDraw.Draw(img)
-
-        if average >= 200:
-            draw.text((captionx, captiony), f'{slidetext[index]}' ,(0,0,0))
-        else:
-            draw.text((captionx, captiony), f'{slidetext[index]}' ,(255,255,255))
-
-        #need to figure out: make text bigger/scalable 
-        
-        img.save(f'slide{index}.png')
-
-        return f'slide{index}.png'
-
+    return f'slide{index}.png'
 
 
 # def addTheme(theme):
